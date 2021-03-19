9497d42d7c2b336490ae77989cfffbd2ef405e9c
nagaeva <[mnomontal@gmail.com]>
2021-03-19 20:08:06 +0300

    new

--- js/FilterComponent.js
+++ js/FilterComponent.js
@@ -1,6 +1,6 @@
 Vue.component('filter-comp', {
-    template: ` <form action="#" class="search-form" @submit.prevent="filter" >
-                    <input type="text" class="search-field" v-model="userSearch" @input="$root.filter()">
+    template: ` <form action="#" class="search-form" @submit.prevent="$parent.filter" >
+                    <input type="text" class="search-field" v-model="$parent.userSearch">
                     <button type="submit" class="btn-search"
                         <i class="fas fa-search"></i>
                     </button>
